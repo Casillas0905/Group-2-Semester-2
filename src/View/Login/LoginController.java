cd2ffce65969569cb7ea70132207c81bab48bbe7
Iker Sanmartin <326322@via.dk>
2022-11-05 22:42:08 +0100

    Some Controllers

---
+++ src/View/Login/LoginController.java
@@ -0,0 +1,18 @@
+package View.Login;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextField;
+
+public class LoginController
+{
+  public TextField mail;
+  public TextField password;
+
+  public void next(ActionEvent actionEvent)
+  {
+  }
+
+  public void reset(ActionEvent actionEvent)
+  {
+  }
+}
