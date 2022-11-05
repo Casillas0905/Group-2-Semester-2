fb060ab2e617c3667944ed6e1333f40bbcb0a8d7
Iker Sanmartin <326322@via.dk>
2022-11-05 23:07:18 +0100

    More controllers edition

---
+++ src/View/Profile/ProfileController.java
@@ -0,0 +1,22 @@
+package View.Profile;
+
+import javafx.event.ActionEvent;
+
+public class ProfileController
+{
+  public void editInfo(ActionEvent actionEvent)
+  {
+  }
+
+  public void manageItems(ActionEvent actionEvent)
+  {
+  }
+
+  public void postItems(ActionEvent actionEvent)
+  {
+  }
+
+  public void wishlist(ActionEvent actionEvent)
+  {
+  }
+}
---
+++ src/View/WishList/WishListController.java
@@ -0,0 +1,5 @@
+package View.WishList;
+
+public class WishListController
+{
+}
